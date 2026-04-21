58cbf4852caf08c3306dccf30caca96a3a670189
Yannick Juarez <yannickjuarez@architech.agency>
2026-04-21 08:51:47 +0200

    Initial Commit

---
+++ StoriesDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StoriesDemo
+//
+//  Created by Yannick Juarez on 21/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StoriesDemo/StoriesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesDemoApp.swift
+//  StoriesDemo
+//
+//  Created by Yannick Juarez on 21/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct StoriesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
